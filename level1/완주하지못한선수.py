27b0fbc8d48ac00079e80630dd3d878aa85dff6d
jeonhl7579 <tpgus19@gmail.com>
2021-03-23 22:30:19 +0900

    2021-03-23 22:30

---
+++ level1/완주하지못한선수.py
@@ -0,0 +1,13 @@
+import sys
+import collections
+input=sys.stdin.readline
+
+
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    for a,b in zip(participant,completion):
+        if a != b:
+            return a
+    return participant.pop()
+
